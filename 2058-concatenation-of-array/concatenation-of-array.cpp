7f33716d398d4f23c033d9f222369c808549c1cc
abdulmoizusman <abdulmoizusman18@gmail.com>
2025-03-09 07:13:30 +0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (81.59%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -5,15 +5,15 @@ public:
         int size = nums.size();
         int double_size = 2*size;
 
-        // vector<int> ans(double_size);
+        vector<int> ans(double_size);
 
 
         for(int i=0; i<size; i++){
-            // ans[i] = ans [i+size] = nums[i];
-            nums.push_back(nums[i]);
+            ans[i] = ans [i+size] = nums[i];
+            // nums.push_back(nums[i]);
         }
 
-        return nums;
+        return ans;
         
     }
 };
